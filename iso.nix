b6b1c66bdb4d5a98d8cd3543bf400350d084a789
Christoph Pilka <c.pilka@asconix.com>
2019-10-06 14:19:37 +0200

    fix: fixed imports

--- iso.nix
+++ iso.nix
@@ -1,9 +1,11 @@
 { config, lib, pkgs, ... }: {
-  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix> ];
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
 
-  # Provide an initial copy of the NixOS channel so that the user
-  # doesn't need to run "nix-channel --update" first.
-  <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    # Provide an initial copy of the NixOS channel so that the user
+    # doesn't need to run "nix-channel --update" first.
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
   
   environment.systemPackages = with pkgs; [ 
     dialog
